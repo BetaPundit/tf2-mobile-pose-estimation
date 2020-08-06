a1a06a183b0e9d5a50804103d62f248f0d9da20d
Aditya Sharma <adityaas26@gmail.com>
2020-08-06 13:46:47 +0530

    Convert CPM model to TPU model

--- model_provider.py
+++ model_provider.py
@@ -11,13 +11,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ======================
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+import os
+
+TPU_WORKER = 'grpc://' + os.environ['COLAB_TPU_ADDR']
+
+strategy = tf.contrib.tpu.TPUDistributionStrategy(
+    tf.contrib.cluster_resolver.TPUClusterResolver(TPU_WORKER))
+
 
 """
 - "simplepose"
 - "mv2_cpm"
 - "mv2_hourglass"
 """
+
 
 def get_model(model_name, model_subname=None, number_of_keypoints=14, config_extra={}):
     if model_name == "simplepose":
@@ -28,40 +37,58 @@ def get_model(model_name, model_subname=None, number_of_keypoints=14, config_ext
         return _get_hourglass_model(model_subname=model_subname, number_of_keypoints=number_of_keypoints, config_extra=config_extra)
     assert False, f"model name is weird: {model_name}"
 
+
 def _get_simplepose_model(model_subname="", number_of_keypoints=14, config_extra={}):
     from models import simplepose_coco
     if model_subname == "mobilenetv2":
         # mv2_alpha: `0.35`,`0.50`,`0.75`,`1.0`,`1.3`,`1.4`
-        model = simplepose_coco.simplepose_mobilenetv2_coco(keypoints=number_of_keypoints, mv2_alpha=1.0)
+        model = simplepose_coco.simplepose_mobilenetv2_coco(
+            keypoints=number_of_keypoints, mv2_alpha=1.0)
     elif model_subname == "mv2":
-        model = simplepose_coco.simplepose_mv2_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_mv2_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resnet18":
-        model = simplepose_coco.simplepose_resnet18_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resnet18_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resnet50b":
-        model = simplepose_coco.simplepose_resnet50b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resnet50b_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resnet101b":
-        model = simplepose_coco.simplepose_resnet101b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resnet101b_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resnet152b":
-        model = simplepose_coco.simplepose_resnet152b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resnet152b_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resneta50b":
-        model = simplepose_coco.simplepose_resneta50b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resneta50b_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resnet101b":
-        model = simplepose_coco.simplepose_resneta101b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resneta101b_coco(
+            keypoints=number_of_keypoints)
     elif model_subname == "resneta152b":
-        model = simplepose_coco.simplepose_resneta152b_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resneta152b_coco(
+            keypoints=number_of_keypoints)
     else:
-        model = simplepose_coco.simplepose_resnet18_coco(keypoints=number_of_keypoints)
+        model = simplepose_coco.simplepose_resnet18_coco(
+            keypoints=number_of_keypoints)
     model.return_heatmap = True
     return model
+
 
 def _get_cpm_model(model_subname="", number_of_keypoints=14, config_extra={}):
     from models import mv2_cpm
     number_of_stages = config_extra["number_of_stages"]
-    return mv2_cpm.ConvolutionalPoseMachine(number_of_keypoints=number_of_keypoints, number_of_stages=number_of_stages)
+    model = mv2_cpm.ConvolutionalPoseMachine(
+        number_of_keypoints=number_of_keypoints, number_of_stages=number_of_stages)
+    tpu_model = tf.contrib.tpu.keras_to_tpu_model(model, strategy=strategy)
+
+    return tpu_model
+
 
 def _get_hourglass_model(model_subname="", number_of_keypoints=14, config_extra={}):
     from models import mv2_hourglass
     return mv2_hourglass.build_mv2_hourglass_model(number_of_keypoints=number_of_keypoints)
+
 
 if __name__ == '__main__':
     my_model = get_model(model_name="simplepose", model_subname="mobilenetv2")
